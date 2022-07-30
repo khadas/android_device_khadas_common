e16a0848299c7c5057397d52ae22aebefb484a75
Goenjoy Huang <goenjoy@khadas.com>
2022-07-30 14:47:53 +0800

    Edge2: add khadas app compile(SchPwrOnOff,KSettings,FactoryTest)
    Change-Id: I8a5d3e9f2812f8a61a8840d55d95d47708d799a0
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,9 @@ PRODUCT_PACKAGES += \
     wakeup-alarmalign-whitelist.xml
 
 PRODUCT_PACKAGES += \
-    FactoryTest
+    FactoryTest \
+    KSettings \
+    SchPwrOnOff
 
 ifdef TARGET_PREBUILT_KERNEL
 # Copy kernel if exists
