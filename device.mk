ccc4cf884e867aefeeb4dd56c531f8ffd6ce09e6
wlq <wlq@rock-chips.com>
2022-10-28 06:51:11 +0000

    set ro.rksdk.version=ANDROID12_RKR12
    Change-Id: I7abd7b52d4d23584972eba54fd7fe684f6aa640d

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ endif
 
 # SDK Version
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rksdk.version=ANDROID$(PLATFORM_VERSION)_RKR11
+    ro.rksdk.version=ANDROID$(PLATFORM_VERSION)_RKR12
 
 TARGET_SYSTEM_PROP += device/khadas/common/build/rockchip/rksdk.prop
 
