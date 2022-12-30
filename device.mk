TinkerBoard-Android/rockchip-android-device-rockchip-rksdk

support aaos car product
Signed-off-by: ZhangHao <zhanghao@rock-chips.com>
Signed-off-by: Luo Wei <lw@rock-chips.com>
Change-Id: I318e0a3d76c788a2378b007b385b99f0348fe782

device.mk
@@ -73,6 +73,8 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), atv)
 
 else ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
   $(call inherit-product, device/rockchip/common/tv/tv_base.mk)
+else ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), car)
+  $(call inherit-product, device/rockchip/common/car/car.mk)
 else ifeq ($(strip $(BUILD_WITH_GO_OPT))|$(strip $(TARGET_ARCH)) ,true|arm)
   # For arm Go tablet.
   $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
@@ -314,7 +316,7 @@ PRODUCT_PACKAGES += \
     librs_jni \
     libjni_pinyinime
 
-ifeq ($(filter atv box, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
+ifeq ($(filter atv box car, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
@@ -858,11 +860,13 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, device/rockchip/common/modules/rockchip_apps.mk)
 
+ifneq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), car)
 ifneq ($(BUILD_WITH_GOOGLE_MARKET), true)
 PRODUCT_PACKAGES += \
     InProcessNetworkStack \
     com.android.tethering.inprocess
 endif
+endif # car without InProcessNetworkStack
 endif # tablet without GMS-Express
 endif
 

khadas/android_device_khadas_common

device.mk
@@ -78,6 +78,8 @@ ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), atv)
 
 else ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), box)
   $(call inherit-product, device/khadas/common/tv/tv_base.mk)
+else ifeq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), car)
+  $(call inherit-product, device/khadas/common/car/car.mk)
 else ifeq ($(strip $(BUILD_WITH_GO_OPT))|$(strip $(TARGET_ARCH)) ,true|arm)
   # For arm Go tablet.
   $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
@@ -319,7 +321,7 @@ PRODUCT_PACKAGES += \
     librs_jni \
     libjni_pinyinime
 
-ifeq ($(filter atv box, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
+ifeq ($(filter atv box car, $(strip $(TARGET_BOARD_PLATFORM_PRODUCT))), )
 # Sensor HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
@@ -864,11 +866,13 @@ PRODUCT_PACKAGES += \
 
 $(call inherit-product, device/khadas/common/modules/rockchip_apps.mk)
 
+ifneq ($(strip $(TARGET_BOARD_PLATFORM_PRODUCT)), car)
 ifneq ($(BUILD_WITH_GOOGLE_MARKET), true)
 PRODUCT_PACKAGES += \
     InProcessNetworkStack \
     com.android.tethering.inprocess
 endif
+endif # car without InProcessNetworkStack
 endif # tablet without GMS-Express
 endif
 