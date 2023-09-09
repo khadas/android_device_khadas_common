7f0ce47a5d2f465a94b288ddb1b167e14330f930
Wu Liangqing <wlq@rock-chips.com>
2023-09-09 08:13:17 +0000

    set ro.rksdk.version=ANDROID13_RKR6
    Change-Id: I102d369278bfd57cd0344cf9f955f5f4f82e53a7

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ endif
 
 # SDK Version
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rksdk.version=ANDROID$(PLATFORM_VERSION)_RKR5
+    ro.rksdk.version=ANDROID$(PLATFORM_VERSION)_RKR6
 
 TARGET_SYSTEM_PROP += device/rockchip/common/build/rockchip/rksdk.prop
 
