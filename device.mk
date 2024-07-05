6e7fcaf60b7eb9ce5dc2f4dae6474b9d85d66551
Xiong Zhang <xiong.zhang@wesion.com>
2024-07-05 19:07:45 +0800

    gapps: add google gapps (Do not compile by default)
    Signed-off-by: Xiong Zhang <xiong.zhang@wesion.com>

--- device.mk
+++ device.mk
@@ -674,6 +674,12 @@ endif # tablet without GMS-Express
 PRODUCT_COPY_FILES += \
    $(LOCAL_PATH)/bootanimation.zip:/system/media/bootanimation.zip
 
+BUILD_WITH_GAPPS_CONFIG :=false
+ifeq ($(BUILD_WITH_GAPPS_CONFIG),true)
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+$(call inherit-product-if-exists, vendor/rockchip/google/gapps.mk)
+endif
+
 # By default, enable zram; experiment can toggle the flag,
 # which takes effect on boot
 PRODUCT_PROPERTY_OVERRIDES += \
